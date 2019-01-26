7255411c46978cdd148149e8df520eb159b9a74c
Main <h_main81423@tss-hr420-dm06.fc.ca>
2019-01-26 16:33:57 -0500

    working on html and js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
 (() => {
- 
+ 	
+		var iconOne = document.querySelector('#icon1');
+
+		
+		var allTheIcons = document.querySelectorAll('.iconSet');
+
+
+		function logIcon(argument) {
+			console.log(this.id);
+			debugger;
+		}
+
+	
+
+		iconOne.addEventListener("click", logIcon);
+
+		allTheIcons.forEach(currentIcon => currentIcon.addEventListener("click", logIcon));
+
+
 })();
