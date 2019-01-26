6053887a8ef9d53afff3af88944c4c36c401dd4a
Main <h_main81423@tss-hr420-dm06.fc.ca>
2019-01-26 14:33:32 -0500

    added more icons to icon set

--- js/main.js
+++ js/main.js
@@ -1,11 +1,3 @@
 (() => {
- 	//self-invoking anonymous function (or Lamda)
- 	var images = document.querySelectorAll('img');
- 	//var => means create a variable
- 	//whatever comes after the equal sign is what the variable
- 	//
- 	//document is always the webpage
- 	//after the dot is always a method or a property
-
- 	console.log(images);
+ 
 })();
